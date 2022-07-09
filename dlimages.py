cd6ff3210989a4caef9f2d7022c6759141ab1348
Victor Prieto <victor.prieto@morado.app>
2022-07-09 20:18:37 -0300

    check if file exists

--- dlimages.py
+++ dlimages.py
@@ -2,6 +2,7 @@ import csv, sys
 import requests
 import urllib3
 import os
+from os.path import exists
 
 filename = 'test.csv'
 # errors='replace'
@@ -13,11 +14,15 @@ with open(filename, 'r', encoding='utf-8') as f:
                 link = row[25]
                 if link.startswith('http'):
                     link = link.split('?')[0]
-                    data = requests.get(link)
-                    print (link)
-                    # Save file data to local copy
-                    with open(os.path.basename(link), 'wb') as file:
-                        file.write(data.content)
+                    filetosave = os.path.basename(link)
+                    if exists(filetosave):
+                        print ("Skipping " + filetosave)
+                    else:
+                        data = requests.get(link)
+                        print (link)
+                        # Save file data to local copy
+                        with open(filetosave, 'wb') as file:
+                            file.write(data.content)
             except:
                 print("Error " + link)
     except csv.Error as e:
